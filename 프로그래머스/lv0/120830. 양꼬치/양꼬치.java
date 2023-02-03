1a9031e181e3afaec501f99ff76dfb1379a36023
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2023-02-03 12:24:47 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,10 +1,5 @@
 class Solution {
-    public int solution(int n, int k) {
-        int answer = 0;
-        int discount =0;
-        answer = (n*12000) + (k*2000);
-        discount = n/10;
-        answer = answer - (discount * 2000);
-        return answer;
+    public int solution(int n, int k) { 
+        return (n*12000) + (k*2000)- ((n/10) * 2000);
     }
 }
