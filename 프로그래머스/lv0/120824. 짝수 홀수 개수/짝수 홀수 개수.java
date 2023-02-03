297a700c2a1e29ebad9ada37481c0808b028b796
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2023-02-03 13:55:44 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0,0};
+        for(int number: num_list) {
+            if(number%2 == 0){
+                answer[0]++;
+            }else answer[1]++;
+        }
+        return answer;
+    }
+}
