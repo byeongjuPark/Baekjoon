108427a2a59c2f7aa0ecff32702b5ebed8f3d2f4
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2023-02-24 14:00:53 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long solution(long num) {
+        int answer = 0;
+        while(true){
+            if(num == 1){
+                break;
+            }
+            answer++;
+            
+            if(num%2 == 0){
+            num = num / 2;
+            }else{
+                num = num * 3 + 1;
+            }
+            
+            if(num == 1){
+                break;
+            }
+            if(answer == 500){
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
