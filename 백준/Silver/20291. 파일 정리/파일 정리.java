d968b0918c97a03fd5f465a67a1bdebec6288646
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2026-03-18 10:54:32 +0900

    [Silver III] Title: 파일 정리, Time: 1240 ms, Memory: 106856 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static class File implements Comparable<File>{
+		int cnt;
+		String fileName;
+		File(int cnt, String fileName){
+			this.cnt = cnt;
+			this.fileName = fileName;
+		}
+		
+		@Override
+		public int compareTo(File f) {
+			return this.fileName.compareTo(f.fileName);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		Map<String, File> map = new TreeMap<>();
+		
+		for(int i = 0 ; i < N; i++) {
+			String str = br.readLine();
+			String arr[] = str.split("\\.");
+			String file = arr[1];
+			map.putIfAbsent(file, new File(0, file));
+			map.get(file).cnt++;
+		}
+		PriorityQueue<File> pq = new PriorityQueue<>();
+		map.forEach((k, v) -> pq.offer(v));
+		while(!pq.isEmpty()) {
+			File cur = pq.poll();
+			System.out.println(cur.fileName+ " " + cur.cnt);
+		}
+	}
+}
