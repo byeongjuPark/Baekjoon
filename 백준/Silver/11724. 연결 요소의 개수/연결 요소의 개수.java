2b964e35aded8e32bd319d0943d937c47ecf2f04
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2026-03-18 10:56:23 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 388 ms, Memory: 115744 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main{
+	static int N, M;
+	static int[] parents;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		make();
+		for(int i= 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			union(a, b);
+		}
+		Set<Integer> componets = new HashSet<>();
+		for(int i = 1; i < N+1; i++) {
+			componets.add(find(i));
+		}
+//		System.out.println(Arrays.toString(parents));
+		System.out.println(componets.size());
+	}
+	static void make() {
+		parents = new int[N+1];
+		for(int i = 0; i < N+1;i++) {
+			parents[i] = i;
+		}
+	}
+	static int find(int a) {
+		if(parents[a] == a) return a;
+		return parents[a] = find(parents[a]);
+	}
+	
+	static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		if(aRoot == bRoot) return false;
+		parents[bRoot] = aRoot;
+		return true;
+	}
+}
