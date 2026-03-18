1c79e3ccbde21c5811c9d81e42c0c6c09eb80f4a
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2026-03-18 11:09:40 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 15696 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,29 +1,28 @@
-
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	static int N;
-	static int dp[];
+//X가 3으로 나누어 떨어지면, 3으로 나눈다.
+//X가 2로 나누어 떨어지면, 2로 나눈다.
+//1을 뺀다.
+// 10 9 3 1
+class Main {
+    static int N;
+    static int dp[];
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N+1];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		dp = new int[N + 1];
-
-		for (int i = 2; i < N + 1; i++) {
-			// 1을 빼는 경우
-			dp[i] = dp[i - 1] + 1;
-			// 2로 나눌 경우
-			if (i % 2 == 0) {
-				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-			}
-			// 3으로 나눌 경
-			if (i % 3 == 0) {
-				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-			}
-		}
-		
-		System.out.println(dp[N]);
-	}
-
+        for(int i = 2; i < N + 1; i++){
+            dp[i] = dp[i-1] + 1;
+            if(i % 2 == 0){
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+            if(i % 3 == 0){
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+        
+        System.out.println(dp[N]);
+    }
 }
