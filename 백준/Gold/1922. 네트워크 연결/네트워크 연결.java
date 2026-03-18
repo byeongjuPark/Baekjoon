5d3097a4583f3f0ccae46c47b0f916cb6c5213cd
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2026-03-18 10:54:01 +0900

    [Gold IV] Title: 네트워크 연결, Time: 404 ms, Memory: 44680 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,74 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] parents;
+	static int V, E;
+	
+	static class Edge implements Comparable<Edge>{
+		int from, to, cost;
+
+		Edge(int from, int to, int cost){
+			this.from = from;
+			this.to = to;
+			this.cost = cost;
+		}
+		@Override
+		public int compareTo(Edge o) {
+			// TODO Auto-generated method stub
+			return this.cost - o.cost;
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		V = Integer.parseInt(br.readLine());
+		E = Integer.parseInt(br.readLine());
+		make();
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		for(int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			pq.add(new Edge(from, to, cost));
+		}
+		int result = 0;
+		while(!pq.isEmpty()) {
+			Edge cur = pq.poll();
+			
+			if(union(cur.from, cur.to)) {
+				result += cur.cost;
+			}
+		}
+		System.out.println(result);
+	}
+	
+	static void make() {
+		parents = new int[V+1];
+		for(int i = 0; i < V+1; i++) {
+			parents[i] = i;
+		}
+	}
+	static int find(int a) {
+		if(parents[a] == a) return a;
+		
+		return parents[a] = find(parents[a]);
+	}
+	
+	static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		if(aRoot == bRoot) return false;
+		
+		parents[aRoot] = bRoot;
+		return true;
+	}
+}
