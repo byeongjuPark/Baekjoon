f66a402027dcfdb1d49c859fea7bdb93a1abfe4e
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2026-03-18 10:55:34 +0900

    [Gold II] Title: 문제집, Time: 400 ms, Memory: 48364 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -1,66 +1,62 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-/**
- *
- * @author bottlepark
- */
-public class Main {
-
-    static int V, E; // Vertex, Edge
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-
-        List<Integer> list = new ArrayList<>();
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] edgeCnt = new int[V + 1];
-        for (int i = 0; i < V + 1; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            edgeCnt[to]++;
-            graph.get(from).add(to);
-        }
-        Queue<Integer> queue = new PriorityQueue<>();
-        // find start
-        for (int i = 1; i < V + 1; i++) {
-            if (edgeCnt[i] == 0) {
-                queue.add(i);
-            }
-        }
-
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            list.add(cur);
-
-            for (int next : graph.get(cur)) {
-                edgeCnt[next]--;
-                if (edgeCnt[next] == 0) {
-                    queue.add(next);
-                }
-            }
-
-        }
-        StringBuilder sb = new StringBuilder();
-
-        for (int i : list) {
-            sb.append(i).append(" ");
-        }
-        System.out.println(sb.toString());
-    }
+public class Main{
+	static int N, M, edgeCnt[];
+	static class Edge {
+		int from;
+		int to;
+		public Edge(int from, int to){
+			this.from = from;
+			this.to = to;
+		}
+	
+	}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		edgeCnt = new int[N+1];
+		ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
+		for(int i = 0; i < N+1; i++) {
+			edges.add(new ArrayList<>());
+		}
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			edges.get(from).add(to);
+			edgeCnt[to]++;
+		}
+		
+		PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+		for(int i = 1; i < N+1; i++) {
+			if(edgeCnt[i] == 0) {
+				q.add(i);
+			}
+		}
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			sb.append(cur).append(" ");
+			
+			for(Integer e: edges.get(cur)){
+				edgeCnt[e]--;
+				if(edgeCnt[e] == 0) {
+					q.add(e);
+				}
+			}
+		}
+		
+		System.out.println(sb);
+		
+	}
 }
