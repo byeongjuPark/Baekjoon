d485b1c8625b0e4d39948c310c52d1a6b2edb0e3
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2026-03-18 10:55:17 +0900

    [Silver III] Title: N과 M (4), Time: 92 ms, Memory: 15080 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,39 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] numbers;
+    static StringBuilder sb;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[M];
+        
+        dfs(0, 1);
+        
+        System.out.println(sb.toString());
+    }
+    
+    static void dfs(int cnt, int start) {
+        if (cnt == M) {
+            for (int i : numbers) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i = start; i <= N; i++) {
+            numbers[cnt] = i;
+            dfs(cnt + 1, i);  // i+1을 i로 수정
+        }
+    }
+}
