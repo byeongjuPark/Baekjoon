a6603e6386d458077cfe65547b732e8892c616e9
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2026-03-18 10:51:43 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        long prev2 = 0; // dp[i-2]
+        long prev1 = 1; // dp[i-1]
+        long current = 1;
+
+        for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        System.out.println(current);
+    }
+}
