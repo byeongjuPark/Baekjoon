cdf15c97baddfc66c34b4460e8fa4b52c210d1df
박병주 <111328143+byeongjuPark@users.noreply.github.com>
2026-03-18 10:54:09 +0900

    [Gold IV] Title: 네트워크 연결, Time: 488 ms, Memory: 45288 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,12 +5,8 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] parents;
-	static int V, E;
-	
 	static class Edge implements Comparable<Edge>{
 		int from, to, cost;
-
 		Edge(int from, int to, int cost){
 			this.from = from;
 			this.to = to;
@@ -19,49 +14,46 @@ public class Main {
 		}
 		@Override
 		public int compareTo(Edge o) {
-			// TODO Auto-generated method stub
 			return this.cost - o.cost;
 		}
-		
 	}
-	
+	static int parents[];
+	static int N, M, result;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-		V = Integer.parseInt(br.readLine());
-		E = Integer.parseInt(br.readLine());
-		make();
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		for(int i = 0; i < E; i++) {
+		result = 0;
+		make();
+		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-			pq.add(new Edge(from, to, cost));
+			
+			pq.offer(new Edge(from, to, cost));
 		}
-		int result = 0;
+		
 		while(!pq.isEmpty()) {
 			Edge cur = pq.poll();
-			
-			if(union(cur.from, cur.to)) {
+			if(union(cur.from, cur.to)) {;
 				result += cur.cost;
 			}
 		}
 		System.out.println(result);
 	}
-	
 	static void make() {
-		parents = new int[V+1];
-		for(int i = 0; i < V+1; i++) {
+		parents = new int[N+1];
+		for(int i = 0 ; i < N+1; i++) {
 			parents[i] = i;
 		}
 	}
 	static int find(int a) {
 		if(parents[a] == a) return a;
-		
 		return find(parents[a]);
 	}
-	
 	static boolean union(int a, int b) {
 		int aRoot = find(a);
 		int bRoot = find(b);
